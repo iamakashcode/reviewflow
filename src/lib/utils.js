0283dd702abeb5a4088641c5f5b2f2f79f676aa2
Akash <akashsachan2001@gmail.com>
2026-05-05 18:00:51 +0530

    bugs

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,6 +9,9 @@ export function toSlug(input) {
 }
 
 export function getBaseUrl() {
+  if (typeof window !== "undefined" && window.location?.origin) {
+    return window.location.origin;
+  }
   if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return "http://localhost:3000";
